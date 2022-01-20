5e44d7671e18d67d0e055e26aaa175a71db5bb4f
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2022-01-20 13:56:00 +0530

    Added Add Task Functionality To Our Smart Contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,29 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
+    // Add A Default Task.
+    constructor() public {
+        createTask("Learn More About Blockchain.");
+    }
+
     uint256 public taskCount = 0;
+
+    // Task Model - Define How Our Task Looks Like.
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    /*
+     Task State.
+     With this we can store our tasks in the blockchain.
+    */
+    mapping(uint256 => Task) public tasks;
+
+    // Method responsible to create a new task.
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
